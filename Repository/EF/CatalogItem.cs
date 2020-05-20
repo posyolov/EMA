0d51f132d518bd0e76c56d756fc32412da52c2b2
posyolov <posyolov@mail.ru>
2020-05-20 15:18:29 +0700

    Initial

---
+++ Repository/EF/CatalogItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Repository.EF
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+        public Guid GlobalId { get; set; }
+    }
+}
---
+++ Repository/EF/EquipmentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repository.EF
+{
+    public class EquipmentContext : DbContext
+    {
+        public EquipmentContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EmaDb;AttachDbFilename='C:\TEMP\PrjVS\_DB\EmaDb.mdf';Trusted_Connection=True;");
+        }
+
+        public DbSet<CatalogItem> Catalog { get; set; }
+    }
+}
