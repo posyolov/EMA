aacafaedb39063a6d9afbd9ee67e62136b39904c
Сергей Посёлов <posyolov@gmail.com>
2020-05-21 23:24:30 +0700

    CatalogVM

--- ViewModel/CatalogVM.cs
+++ ViewModel/CatalogVM.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Repository.EF;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ViewModel
 {
     public class CatalogVM
     {
+        public ObservableCollection<CatalogItem> CatalogItems { get; }
+
+        public CatalogVM()
+        {
+            CatalogItems = new ObservableCollection<CatalogItem>();
+            CatalogItems.Add(new CatalogItem { Id = 1, GlobalId = new Guid(), Vendor = new Vendor { Name = "Siemens" }, ProductCode = "111", Title = "aaa" });
+            CatalogItems.Add(new CatalogItem { Id = 2, GlobalId = new Guid(), Vendor = new Vendor { Name = "Sick" }, ProductCode = "222", Title = "bbb" });
+        }
     }
 }
