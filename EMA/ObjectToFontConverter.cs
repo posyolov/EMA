7449bd39598a680d2c0b4eedcc43804f8a3649e5
Сергей Посёлов <posyolov@gmail.com>
2020-07-29 00:12:59 +0700

    objectToFontConverter

---
+++ EMA/ObjectToFontConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace EMA
+{
+    public class ObjectToFontConverter : IValueConverter
+    {
+        const double REGULAR_FONT = 12, CAPITAL_FONT = 14;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? REGULAR_FONT : CAPITAL_FONT; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
