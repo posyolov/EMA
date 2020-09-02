f36b48a3d35b666b1a8d3c93ead9507532dc90ff
Сергей Посёлов <posyolov@gmail.com>
2020-09-02 19:31:46 +0700

    Initial

---
+++ WebEMA/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Repository;
+
+namespace WebEMA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEntityManager<CatalogItem> catalogManager;
+
+        public HomeController(IEntityManager<CatalogItem> catalogManager)
+        {
+            this.catalogManager = catalogManager;
+        }
+
+        public IActionResult Index()
+        {
+            var catalog = catalogManager.Get();
+            return View(catalog);
+        }
+    }
+}
