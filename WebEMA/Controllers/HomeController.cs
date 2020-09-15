f62671d1b2020f8e1025c29a0198d142e1fe96bb
Сергей Посёлов <posyolov@gmail.com>
2020-09-16 00:45:04 +0700

    PositionData using Ajax

--- WebEMA/Controllers/HomeController.cs
+++ WebEMA/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Model;
 using Repository;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WebEMA.Controllers
 {
@@ -13,18 +11,27 @@ namespace WebEMA.Controllers
         private readonly IEntityManager<Position> positionManager;
         private readonly IEntityManager<CatalogItem> catalogManager;
 
+        IEnumerable<Position> positions;
+
         public HomeController(IEntityManager<Position> positionManager, IEntityManager<CatalogItem> catalogManager)
         {
             this.positionManager = positionManager;
             this.catalogManager = catalogManager;
+
+            positions = positionManager.Get();
         }
 
         public IActionResult Index()
         {
-            ViewBag.PostionsTreeVM = positionManager.Get();
+            ViewBag.PostionsTreeVM = positions;
             ViewBag.CatalogVM = catalogManager.Get();
             return View();
         }
 
+        public IActionResult PositionData(int id)
+        {
+            Position pos = positions.FirstOrDefault(p => p.Id == id);
+            return PartialView("../Partial/_PositionData", pos);
+        }
     }
 }
